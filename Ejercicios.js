94fb9a9db20663f5f2f1781ed649fcba85a3e1e4
JhonnierPeraltaR <jsperaltar@libertadores.edu.co>
2025-03-29 09:31:35 -0500

    Subiendo al repo

---
+++ Ejercicios.js
@@ -0,0 +1,29 @@
+console.log(eval(new String("2+2")));
+console.log('\n');
+console.log(eval("2+2"));
+
+var nombre = "Hola Mundo";
+console.log(nombre);
+
+var variable = "nombre";
+
+let nombre2 = "nombre";
+nombre2 = "Camilo";
+const apellido = "Rocha";
+
+console.log(nombre2);
+console.log(variable);
+console.log(apellido);
+
+let nombrecompleto1 = "Sebastian Vanegas";
+let nombreCompleto1 = "Camilo Rocha";
+
+console.log(nombrecompleto1);
+console.log(nombreCompleto1);
+
+let _1654 = 111;
+let $money = "dolar";
+let aiuda = "Tengo suenio profe";
+let a,b;
+a = 5;
+b = "America de Cali";
